922271259a3c8048198f7ce0f48eada593166818
Richson-Tech <folorunshoahmedolajide@gmail.com>
2024-06-01 19:30:53 +0100

    new commits

--- src/actions.ts
+++ src/actions.ts
@@ -1,3 +1,5 @@
-export const getSession = async ()=>{}
+import { sessionOptions, SessionData } from "@/lib"
+
+export const getSession = async () => { }
 export const login = async ()=>{}
 export const logout = async ()=>{}
