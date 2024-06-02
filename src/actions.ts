c3ccc576986be67b6e7a965509f66fad0c96a835
Richson-Tech <folorunshoahmedolajide@gmail.com>
2024-06-02 23:57:54 +0100

    new commits

--- src/actions.ts
+++ src/actions.ts
@@ -2,7 +2,8 @@ import { sessionOptions, SessionData } from "@/lib"
 import { getIronSession } from "iron-session"
 
 export const getSession = async () => { 
-    const session = await getIronSession<SessionData>
+    const session = await getIronSession<SessionData>(cookies(), sessionOptions);
+    return session;
 }
 export const login = async ()=>{}
 export const logout = async ()=>{}
