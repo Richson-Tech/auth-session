1bd441a61c9b1eb468ff0fa0659d22b48f5de130
Richson-Tech <folorunshoahmedolajide@gmail.com>
2024-06-03 23:58:54 +0100

    new commits

--- src/actions.ts
+++ src/actions.ts
@@ -8,9 +8,5 @@ export const getSession = async () => {
     const session = await getIronSession<SessionData>(cookies(), sessionOptions);
     return session;
 }
-export const login = async () => {
-  
-}
-export const logout = async () => {
-  
-}
+export const login = async () => {}
+export const logout = async () => {}
