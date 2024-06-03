44d788fe494ff91a3451447f18eff1e701565e44
Richson-Tech <folorunshoahmedolajide@gmail.com>
2024-06-03 23:58:21 +0100

    new commits

--- src/actions.ts
+++ src/actions.ts
@@ -1,9 +1,10 @@
+"use server"
 import { sessionOptions, SessionData } from "@/lib"
 import { getIronSession } from "iron-session"
 import { cookies } from "next/headers";
 
 export const getSession = async () => { 
-    "use server"
+   
     const session = await getIronSession<SessionData>(cookies(), sessionOptions);
     return session;
 }
