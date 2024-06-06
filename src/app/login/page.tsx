7feea0d2076c006a320607e4a24a2ad91c5e4a7f
Richson-Tech <folorunshoahmedolajide@gmail.com>
2024-06-06 23:01:39 +0100

    new commits

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -1,9 +1,11 @@
+import LoginForm from "@/components/LoginForm";
 import React from "react";
 
 const LoginPage = () => {
   return (
     <div className="login">
       <h1>Welcome to the LoginPage</h1>
+      <LoginForm/>
     </div>
   );
 };
