d5e226d22853a26f7aab518c6909c1472fb69b7a
Richson-Tech <folorunshoahmedolajide@gmail.com>
2024-06-06 23:00:41 +0100

    new commits

--- src/components/LoginForm.tsx
+++ src/components/LoginForm.tsx
@@ -1,12 +1,12 @@
-import React from 'react'
+import React from "react";
 
 const LoginForm = () => {
   return (
-      <form>
-          <input type="text" name='username' required placeholder='username'/>
-          <input type="password" name='password' required placeholder='password'/>
+    <form>
+      <input type="text" name="username" required placeholder="username" />
+      <input type="password" name="password" required placeholder="password" />
     </form>
-  )
-}
+  );
+};
 
-export default LoginForm
+export default LoginForm;
