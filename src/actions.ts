cf4cc2cf547f41db52b518232f26187e10fa25f6
Richson-Tech <folorunshoahmedolajide@gmail.com>
2024-06-06 22:47:04 +0100

    new commits

--- src/actions.ts
+++ src/actions.ts
@@ -4,10 +4,10 @@ import { getIronSession } from "iron-session";
 import { cookies } from "next/headers";
 
 export const getSession = async () => {
-    const session = await getIronSession<SessionData>(cookies(), sessionOptions);
-    if (!session.isLoggedIn) {
-        session.isLoggedIn = defaultSession.isLoggedIn;
-    }
+  const session = await getIronSession<SessionData>(cookies(), sessionOptions);
+  if (!session.isLoggedIn) {
+    session.isLoggedIn = defaultSession.isLoggedIn;
+  }
 
   return session;
 };
