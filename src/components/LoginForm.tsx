0242a9d34f9e902d6a573a5730986af1c356533d
Richson-Tech <folorunshoahmedolajide@gmail.com>
2024-06-06 23:01:11 +0100

    new commits

--- src/components/LoginForm.tsx
+++ src/components/LoginForm.tsx
@@ -5,6 +5,7 @@ const LoginForm = () => {
     <form>
       <input type="text" name="username" required placeholder="username" />
       <input type="password" name="password" required placeholder="password" />
+      <button>Login</button>
     </form>
   );
 };
