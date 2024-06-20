7a41db93cdcf8096ad30a106270c92a58a0b18da
Richson-Tech <folorunshoahmedolajide@gmail.com>
2024-06-20 21:52:55 +0100

    new commits

--- src/app/premium/page.tsx
+++ src/app/premium/page.tsx
@@ -8,6 +8,14 @@ const PremiumPage = async () => {
   if (!session.isLoggedIn) {
     redirect("/");
   }
+
+  if (!session.isPro) {
+    return (
+      <div className="notPremium">
+        <h1>Only premium users can see the content!</h1>
+      </div>
+    );
+  }
   return (
     <div className="premium">
       <h1>Welcome to the PremiumPage</h1>
