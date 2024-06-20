b0825d747cb5e8601e5a61a5eea94b7b3da0368b
Richson-Tech <folorunshoahmedolajide@gmail.com>
2024-06-20 21:51:08 +0100

    new commits

--- src/app/premium/page.tsx
+++ src/app/premium/page.tsx
@@ -1,9 +1,14 @@
 import { getSession } from "@/actions";
+import { redirect } from "next/navigation";
 import React from "react";
 
 const PremiumPage = async () => {
 
   const session = await getSession()
+
+  if (!session.isLoggedIn) {
+  redirect("/")
+  }
   return (
     <div className="premium">
       <h1>Welcome to the PremiumPage</h1>
